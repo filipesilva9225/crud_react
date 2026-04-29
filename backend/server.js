aee1de590fbb895004d14ee6d896d9ba098c6c2f
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2026-04-29 21:15:34 +0000

    adding get

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/users", async(req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.listen(3001);
